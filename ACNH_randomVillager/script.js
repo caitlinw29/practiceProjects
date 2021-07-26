164fc5c2b1c2b020108876060aa0bf3e91831327
Caitlin Wall <caitlinwall96@gmail.com>
2021-07-25 20:43:23 -0400

    Add functioning JavaScript to page and update CSS/HTML

---
+++ ACNH_randomVillager/script.js
@@ -0,0 +1,44 @@
+function genRanVillager() 
+{ 
+   var imageArray = [  
+        {src: "Images/ACNH_VillagerPhotos/Admiral.png"},   
+        {src: "Images/ACNH_VillagerPhotos/Agent_S.png"},
+        {src: "Images/ACNH_VillagerPhotos/Agnes.png"}];  
+
+    var arrayLength = imageArray.length;  
+    var newArray = [];  
+
+    for (var i = 0; i < arrayLength; i++)   
+       {  
+            newArray[i] = new Image();  
+            newArray[i].src = imageArray[i].src; 
+        }  
+
+    function getRandomNum()   
+    {  
+        // generate and return a random number for the image to be displayed   
+        imgNo = Math.floor(Math.random() * arrayLength);  
+        return newArray[imgNo];  
+    }
+    
+    var newImage = getRandomNum(0, newArray.length - 1);
+
+    // remove the previous images  
+    var images = document.getElementsByTagName('img');  
+    var l = images.length;  
+    for (var p = 0; p < l; p++) {  
+        images[0].parentNode.removeChild(images[0]);  
+    }  
+
+    // display the new random image    
+    document.getElementById("randomImg").append(newImage);  
+} 
+
+
+
+
+
+
+
+
+
